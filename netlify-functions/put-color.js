d1270630ce3dd52c1627420fb875cf17293de3db
Pete Hodgson <git@thepete.net>
2019-09-19 21:31:09 -0700

    tweak function

--- netlify-functions/put-color.js
+++ netlify-functions/put-color.js
@@ -1,13 +1,13 @@
 exports.handler = async event => {
-    console.dir({event});
-    
+    console.log({event});
+
     const color = event.body.color;
     console.log('updating color:', color);
 
     return {
         statusCode: 200,
-        body: {
+        body: JSON.stringify({
             color: 'rebeccapurple'
-        }
+        })
     };
 }
